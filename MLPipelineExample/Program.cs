fa9352179a7a3b6fe8cc4eacdac0cf84fdf52194
Alex Peterson <petersonalex@seattleu.edu>
2021-05-23 13:40:27 -0700

    Add project files.

---
+++ MLPipelineExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MLPipelineExample
+{
+    /// <summary>
+    /// This project will demonstrate how to create a logistic
+    /// regression model from read in json data using ML.NET. 
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
