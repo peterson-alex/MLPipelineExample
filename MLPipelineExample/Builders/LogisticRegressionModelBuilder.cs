2731a3153a5129379d9bd17bfb57bc1caa4695a1
Alex Peterson <petersonalex@seattleu.edu>
2021-05-26 20:56:31 -0700

    Add LogisticRegressionModelBuilder class.

---
+++ MLPipelineExample/Builders/LogisticRegressionModelBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLPipelineExample.Builders
+{
+    /// <summary>
+    /// Builds a logistic regression model in ML.NET.
+    /// </summary>
+    public class LogisticRegressionModelBuilder
+    {
+    }
+}
