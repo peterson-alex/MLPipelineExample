d695e471f1656820a4f8da70b6a2fee4714adcb9
Alex Peterson <petersonalex@seattleu.edu>
2021-05-30 08:53:43 -0700

    Add LoadTrainingData() method to LogisticRegressionModelBuilder class.

--- MLPipelineExample/Builders/LogisticRegressionModelBuilder.cs
+++ MLPipelineExample/Builders/LogisticRegressionModelBuilder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ML;
+using MLPipelineExample.Models;
 
 namespace MLPipelineExample.Builders
 {
@@ -15,6 +16,7 @@ namespace MLPipelineExample.Builders
     {
 
         private MLContext _context; // ML context used to train and build model
+        private IDataView _trainingData; // training data used to train model
         
         /// <summary>
         /// Default constructor. 
@@ -31,6 +33,17 @@ namespace MLPipelineExample.Builders
         public LogisticRegressionModelBuilder(MLContext context)
         {
             _context = context;
+        }
+
+        /// <summary>
+        /// Loads the training data into the 
+        /// </summary>
+        /// <param name="imageResults"></param>
+        /// <returns></returns>
+        public IDataView LoadTrainingData(IEnumerable<ImageResultInputModel> imageResults)
+        {
+            _trainingData = _context.Data.LoadFromEnumerable(imageResults);
+            return _trainingData; 
         }
     }
 }
