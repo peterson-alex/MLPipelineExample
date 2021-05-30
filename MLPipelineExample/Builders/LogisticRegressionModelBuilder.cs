364e7ad6abf9e7fb5acecd5b086e1642898027c4
Alex Peterson <petersonalex@seattleu.edu>
2021-05-30 08:45:53 -0700

    Add constructors to LogisticRegressionModelBuilder.

--- MLPipelineExample/Builders/LogisticRegressionModelBuilder.cs
+++ MLPipelineExample/Builders/LogisticRegressionModelBuilder.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML;
 
 namespace MLPipelineExample.Builders
 {
+
     /// <summary>
     /// Builds a logistic regression model in ML.NET.
     /// </summary>
     public class LogisticRegressionModelBuilder
     {
+
+        private MLContext _context; // ML context used to train and build model
+        
+        /// <summary>
+        /// Default constructor. 
+        /// </summary>
+        public LogisticRegressionModelBuilder()
+        {
+            _context = new MLContext(); 
+        }
+
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        /// <param name="context"></param>
+        public LogisticRegressionModelBuilder(MLContext context)
+        {
+            _context = context;
+        }
     }
 }
