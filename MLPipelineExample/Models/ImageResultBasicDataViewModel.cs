f9d391a9dd6495aa1ee8e15079aa6375cd2b7803
Alex Peterson <petersonalex@seattleu.edu>
2021-05-31 07:30:48 -0700

    Add ImageResultBasicDataViewModel.

---
+++ MLPipelineExample/Models/ImageResultBasicDataViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLPipelineExample.Models
+{
+    /// <summary>
+    /// View model that maps onto the shape of the json 
+    /// data that will be consumed by the pipeline.
+    /// </summary>
+    public class ImageResultBasicDataViewModel
+    {
+        /// <summary>
+        /// The list of ImageResultBasicViewModels
+        /// </summary>
+        public List<ImageResultBasicViewModel> ImageResultBasicViewModels { get; set; }
+    }
+}
