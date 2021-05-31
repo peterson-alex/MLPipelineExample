a892b38dad26b4fd4afa1b7d41c649e37de87353
Alex Peterson <petersonalex@seattleu.edu>
2021-05-31 11:35:14 -0700

    Add BinaryClassifierMetaDataModel.

---
+++ MLPipelineExample/Models/BinaryClassifierMetaDataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLPipelineExample.Models
+{
+    /// <summary>
+    /// Holds meta data for a binary classification model, 
+    /// including the model metrics.
+    /// </summary>
+    public class BinaryClassifierMetaDataModel
+    {
+        /// <summary>
+        /// The Date + time that the model was generated.
+        /// </summary>
+        public DateTime ModelGeneratedDateTime { get; set; } = DateTime.Now;
+
+        // other metadata goes here
+
+        /// <summary>
+        /// The metrics used to evaluate the model. 
+        /// </summary>
+        public BinaryClassificationMetricsModel Metrics { get; set; }
+    }
+}
