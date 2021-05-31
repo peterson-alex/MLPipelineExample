7963d254b322321fe49cd010040829e036a68d47
Alex Peterson <petersonalex@seattleu.edu>
2021-05-31 09:58:26 -0700

    Add BinaryClassificationMetricsModel.

---
+++ MLPipelineExample/Models/BinaryClassificationMetricsModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLPipelineExample.Models
+{
+    /// <summary>
+    /// Holds all of the evaluation metrics for a 
+    /// binary classification model. 
+    /// </summary>
+    public class BinaryClassificationMetricsModel
+    {
+        /// <summary>
+        /// Accuracy of the classifier. The closer to 
+        /// 1.00, the better, provided the data sample is 
+        /// balanced between the two possible classes.
+        /// </summary>
+        public double Accuracy { get; set; }
+
+        /// <summary>
+        /// The area under the precision recall curve. Values closer 
+        /// to 1.00 indicate high precision and high recall.
+        /// </summary>
+        public double AreaUnderPrecisionRecallCurve { get; set; }
+
+        /// <summary>
+        /// Area under the Roc (receiving operating characeristic)
+        /// curve. The closer to 1.00, the better. If under .50, the 
+        /// model is worthless.
+        /// </summary>
+        public double AreaUnderRocCurve { get; set; }
+
+        /// <summary>
+        /// F1 score. The F1 score represents a balance between precision
+        /// and recall. The closer to 1.00, the better.
+        /// </summary>
+        public double F1Score { get; set; }
+
+        /// <summary>
+        /// The ratio of true positives divided by total 
+        /// predicted positives.
+        /// </summary>
+        public double PositivePrecision { get; set; }
+
+        /// <summary>
+        /// The ratio of true negatives divided by total 
+        /// predicted negatives.
+        /// </summary>
+        public double NegativePrecision { get; set; }
+        
+        /// <summary>
+        /// The ratio of true positives divided by total 
+        /// actual positives.
+        /// </summary>
+        public double PositiveRecall { get; set; }
+
+        /// <summary>
+        /// The ratio of true negatives divided by total
+        /// actual negatives.
+        /// </summary>
+        public double NegativeRecall { get; set; }
+        
+        /// <summary>
+        /// The number of true positives in the test data.
+        /// </summary>
+        public int TruePositiveCount { get; set; }
+
+        /// <summary>
+        /// The number of false positives in the test data.
+        /// </summary>
+        public int FalsePositiveCount { get; set; }
+
+        /// <summary>
+        /// The number of true negatives in the test data.
+        /// </summary>
+        public int TrueNegativeCount { get; set; }
+        
+        /// <summary>
+        /// The number of false negatives in the test data.
+        /// </summary>
+        public int FalseNegativeCount { get; set; }
+    }
+}
