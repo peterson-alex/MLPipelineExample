0e33f75ef6d4f0499191e756d50aeb1e1ec9b075
Alex Peterson <petersonalex@seattleu.edu>
2021-06-05 16:53:51 -0700

    Add SavedModelPathModel.cs.

---
+++ MLPipelineExample/Models/SavedModelPathModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLPipelineExample.Models
+{
+    /// <summary>
+    /// Stores the full paths of the saved model and the 
+    /// model meta data file.
+    /// </summary>
+    public class SavedModelPathModel
+    {
+        /// <summary>
+        /// The full path to the model (.zip file).
+        /// </summary>
+        public string ModelPath { get; set; }
+
+        /// <summary>
+        /// The full path to the meta data file (.json file).
+        /// </summary>
+        public string MetaDataPath { get; set; }
+    }
+}
