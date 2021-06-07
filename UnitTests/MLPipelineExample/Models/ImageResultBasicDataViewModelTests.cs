05caf2238d29957078271f0ba3d947f67d6e041f
Alex Peterson <petersonalex@seattleu.edu>
2021-06-06 17:12:44 -0700

    Add ImageResultBasicDataViewModelTests.cs.

---
+++ UnitTests/MLPipelineExample/Models/ImageResultBasicDataViewModelTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MLPipelineExample.Models;
+using NUnit.Framework;
+
+namespace UnitTests.MLPipelineExample.Models
+{
+    /// <summary>
+    /// Unit tests for ImageResultBasicDataViewModel.
+    /// </summary>
+    public class ImageResultBasicDataViewModelTests
+    {
+        [Test]
+        public void ImageResultBasicDataViewModel_Default_Not_Null()
+        {
+            // Act
+            var item = new ImageResultBasicDataViewModel();
+
+            // Assert
+            Assert.NotNull(item);
+        }
+
+        [Test]
+        public void ImageResultBasicDataViewModel_Default_Properties_Null()
+        {
+            // Act
+            var item = new ImageResultBasicDataViewModel();
+
+            // Assert
+            Assert.AreEqual(item.ImageResultBasicViewModels, null);
+        }
+
+        [Test]
+        public void ImageResultBasicDataViewModel_Set_Properties_Not_Null()
+        {
+            // Arrange
+            var imageResults = new List<ImageResultBasicViewModel>()
+            { 
+                new ImageResultBasicViewModel() 
+                { 
+                    Value = 1.0f, 
+                    UserID = "bogus",
+                    ReadingSuccess = true
+                }
+            };
+
+            // Act
+            var item = new ImageResultBasicDataViewModel()
+            {
+                ImageResultBasicViewModels = imageResults
+            };
+
+            // Assert
+            Assert.NotNull(item.ImageResultBasicViewModels);
+        }
+    }
+}
