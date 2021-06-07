d8179c4994b5e9597fbb7d4079a6acdcbe48f0c9
Alex Peterson <petersonalex@seattleu.edu>
2021-06-06 17:49:20 -0700

    Add ModelInput.cs.

---
+++ ModelConsumer/ModelInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelConsumer
+{
+    public class ModelInput
+    {
+        /// <summary>
+        /// The bilirubin value of the reading.
+        /// </summary>
+        public float Value { get; set; }
+
+        /// <summary>
+        /// The user that took the reading.
+        /// </summary>
+        public string UserID { get; set; }
+
+        /// <summary>
+        /// Whether the reading was declared successful
+        /// or not.
+        /// </summary>
+        public bool ReadingSuccess { get; set; }
+    }
+}
