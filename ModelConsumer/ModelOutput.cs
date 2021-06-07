e7cef59d02bdb876adadc29179703fdbdfb5f117
Alex Peterson <petersonalex@seattleu.edu>
2021-06-06 17:55:18 -0700

    Add ModelOutput.cs.

---
+++ ModelConsumer/ModelOutput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace ModelConsumer
+{
+    public class ModelOutput
+    {
+        /// <summary>
+        /// The predicted value for the given 
+        /// input. 
+        /// </summary>
+        [ColumnName("ReadingSuccess")]
+        public bool Prediction { get; set; }
+
+        /// <summary>
+        /// The f1 score associated with this prediction.
+        /// </summary>
+        public float[] Score { get; set; }
+    }
+}
