3837cefa7de658f80d33821acfb6f62000066601
Alex Peterson <petersonalex@seattleu.edu>
2021-06-07 12:44:12 -0700

    Add test for default property values after LogisticRegressionModelBuilder initialization.

--- UnitTests/MLPipelineExample/Builders/LogisticRegressionModelBuilderTests.cs
+++ UnitTests/MLPipelineExample/Builders/LogisticRegressionModelBuilderTests.cs
@@ -25,5 +25,29 @@ namespace UnitTests.MLPipelineExample.Builders
             // Assert
             Assert.NotNull(builder);
         }
+
+        /// <summary>
+        /// Tests each public property to ensure that they hold 
+        /// expected values after object initialization.
+        /// </summary>
+        [Test]
+        public void Constructor_Default_Properties_Valid()
+        {
+            // Act
+            var builder = new LogisticRegressionModelBuilder();
+
+            // Assert
+            Assert.NotNull(builder.Context);
+            Assert.AreEqual(builder.CategoricalVariableConverter, null);
+            Assert.AreEqual(builder.FeatureVariableConcatenator, null);
+            Assert.AreEqual(builder.DataSet, null);
+            Assert.AreEqual(builder.Label, null);
+            Assert.NotNull(builder.TrainingOptions);
+            Assert.AreEqual(builder.TrainingOptions.FeatureColumnName, "Features");
+            Assert.AreEqual(builder.TrainingOptions.MaximumNumberOfIterations, 100);
+            Assert.AreEqual(builder.TrainingOptions.OptimizationTolerance, 1e-8f);
+            Assert.AreEqual(builder.TrainedModel, null);
+            Assert.AreEqual(builder.BinaryClassificationMetrics, null);
+        }
     }
 }
